f36d5c6f33bec0be3aff2e59f52343aa99c25e6f
kevin246 <kevinle246@gmail.com>
2023-10-31 20:01:24 -0700

    updates to metadata combining

--- scripts/metadata_combining.R
+++ scripts/metadata_combining.R
@@ -4,13 +4,17 @@ anemia_metadata <- read_excel("metadata/anemia_metadata.xlsx")
 infant_metadata <- read_excel("metadata/infant_metadata.xlsx")
 
 anemia_cols <- anemia_metadata %>%
-  select(`#SampleID`, "age_months", "sex", ) %>%
+  filter(age_months == 6, grepl("BM", diet, fixed=TRUE))%>%
+  select(`#SampleID`, "age_months", "sex", "diet") %>%
   mutate(cohort = "anemia") %>%
   mutate(sex = recode(sex, `M` = "male", `F` = "female")) %>%
   rename(age = age_months)
 
 infant_cols <- infant_metadata %>%
-  select(`#SampleID`, "age_category", "sex") %>%
+  filter(age_category == "6 months") %>%
+  filter(feed == "combined" | feed == "breast") %>%
+  rename(diet = feed) %>%
+  select(`#SampleID`, "age_category", "sex", "diet") %>%
   mutate(cohort = "infant") %>%
   rename(age = age_category) %>%
   mutate(age = word(age , 1  , -2))
